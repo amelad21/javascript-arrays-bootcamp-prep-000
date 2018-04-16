a7906ac143383632a2a182030e98149cf5e0fb33
dervishiamela@gmail.com Dervishi <dervishiamela@gmail.com>
2018-04-16 23:49:59 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+ function addElementToBeginningOfArray(chocolateBars) {
+ 
+  chocolateBars =  ['Mars',1];
+ }
+   
+   
+ 
+ function destructivelyAddElementToBeginningOfArray(chocolateBars) {
+   chocolateBars.unshift = ["foo",1]
+ }
